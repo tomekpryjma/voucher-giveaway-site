3db26db7a617bec73c491aa247dbb7d734fc4d71
Tomek Pryjma <tomek.pryjma@gmail.com>
2022-07-30 16:24:45 +0100

    Add voucher index

---
+++ resources/js/Pages/Voucher/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-4xl text-center">Available codes</h1>
+
+        <div class="container mx-auto max-w-3xl space-y-3">
+            <Link
+                v-for="(voucher, index) in this.data.vouchers"
+                :key="index"
+                :href="this.routes['voucher.view'] + '/' + voucher.id"
+                class="block p-5 rounded w-full border-2"
+            >
+                <p v-if="voucherPostedByMe(voucher)">This one is yours!</p>
+
+                {{ voucher.code }}
+            </Link>
+        </div>
+    </div>
+</template>
+<script>
+import MainLayoutVue from '../../Layouts/MainLayout.vue';
+
+export default {
+    layout: MainLayoutVue,
+
+    props: ['data', 'routes'],
+}
+</script>
