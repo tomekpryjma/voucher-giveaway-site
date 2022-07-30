da904eec60f62b49e3d551b38f8fd1e05c7d53e6
Tomek Pryjma <tomek.pryjma@gmail.com>
2022-07-30 16:25:29 +0100

    Add voucher view view

---
+++ resources/js/Pages/Voucher/View.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto">
+        <h1>{{ this.data.voucher.code }}</h1>
+        <p v-if="voucherPostedByMe(this.data.voucher)">You posted this voucher! :)</p>
+        <p v-else>This voucher was kindly posted by {{ this.data.voucher.posted_by.name }}</p>
+    </div>
+</template>
+<script>
+import MainLayoutVue from '../../Layouts/MainLayout.vue';
+
+export default {
+    layout: MainLayoutVue,
+    props: ['data']
+}
+</script>
